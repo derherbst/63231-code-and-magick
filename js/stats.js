ab3c52eb6261d41ddd287cb8e7a6d3e682d69861
Georgy Gerbst <gerbst.g@gmail.com>
2017-01-31 22:51:29 +0300

    Временный коммит

--- js/stats.js
+++ js/stats.js
@@ -1,34 +1,55 @@
-'use strict'
+'use strict';
+
+var namesArray = ['Вы', 'Олег', 'Саша', 'Юля'];
+var timesArray = [500, 235, 345, 400];
 
 window.renderStatistics = function (ctx, names, times) {
-  var canvas = document.querySelector('canvas');
-  var defineCtx = canvas.getContext('2d');
-  
+
+  var histHeight = 150;
+  var histX = 150;
+  var colWidth = 40;
+  var colBetween = 50;
+  var mineColor = 'rgba(255, 0, 0, 1)';
+
   ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
   ctx.fillRect(110, 20, 420, 270);
-  
+
   ctx.fillStyle = 'white';
   ctx.fillRect(100, 10, 420, 270);
-  
+
   ctx.fillStyle = '#000';
   ctx.font = '16px PT Mono';
   ctx.textBaseline = 'hanging';
   ctx.fillText('Ура вы победили!', 120, 40);
   ctx.fillText('Список результатов:', 120, 60);
-  
-  
-  var histHeight = 150;
-  var colWidth = 40;
-  var colBetween = 50px;
-  
-  var max = 0;
-  var min = Infinity;
-  
-  for(var i = 0; i < times.length; i++) {
+
+  for (var i = 0; i < timesArray.length; i++) {
+
+    var max = 0;
+    var min = Infinity;
+
     if (times[i] > max) {
-      
+      max = times[i];
     }
+
+    if (times[i] < min) {
+      min = times[i];
+    }
+
+    var step = histHeight / max;
+    var colHeight = step * times[i];
+
+    if (namesArray[i] === 'Вы') {
+      ctx.fillStyle = mineColor;
+    } else {
+      ctx.fillStyle = ['rgb(0, 0, ' + Math.round(80 + Math.random() * (255 - 80)) + ')'];
+    }
+
+    ctx.fillRect(histX + colBetween * i, 100 + histHeight - colHeight, colWidth, colHeight);
+    ctx.fillStyle = '#000';
+    ctx.fillText(namesArray[i], histX + colBetween * i, 100 + histHeight);
   }
 };
 
-renderStatistics(defineCtx, ['Олег', 'Саша', 'Юля', 'Вадим', 'Вы'], )
+// var canvas = document.querySelector('canvas');
+//  renderStatistics(canvas.getContext('2d'), namesArray, timesArray);
